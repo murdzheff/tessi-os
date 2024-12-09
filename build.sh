58d4b6eefd77681e0cc1e196f8365de43dc92ede
murdzheff <murdzheff@gmail.com>
2024-12-09 09:10:19 +0200

    adding apps script from previous repo

--- build.sh
+++ build.sh
@@ -12,8 +12,34 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-rpm-ostree install screen
+# this installns a package from fedora repos
+# Define an array of package names
+packages=(
+    papirus-icon-theme
+    remmina
+    kitty
+    yum-utils
+    nss-tools
+    gparted
+    foliate
+    freerdp
+    neovim
+    webkit2gtk3
+    htop
+    afetch
+    # Add more packages as needed
+)
+
+# Loop through the array and install each package
+for package in "${packages[@]}"; do
+    echo "Installing $package..."
+    rpm-ostree install "$package"
+    if [ $? -eq 0 ]; then
+        echo "$package installed successfully."
+    else
+        echo "Failed to install $package."
+    fi
+done
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
