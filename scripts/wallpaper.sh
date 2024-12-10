7a0a75889bb9493f3daad6f47d5f15ac77db90d2
Georgi Murdzheff <120983342+murdzheff@users.noreply.github.com>
2024-12-11 00:21:09 +0200

    Update wallpaper.sh

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# Check if SOURCE_IMAGE is set, and if not, use a default value
+: ${SOURCE_IMAGE:="kinno-main"}  # Default to "default-image" if SOURCE_IMAGE is not set
+
+echo "Source image: $SOURCE_IMAGE"
+
 
 set -ouex pipefail
 
@@ -9,20 +14,19 @@ cp /tmp/scripts/files/5275499.jpg /usr/share/backgrounds/default.png
 # Assume SOURCE_IMAGE is passed as the first argument or set as an environment variable
 SOURCE_IMAGE=${1:-${SOURCE_IMAGE}}
 
-# Template for the if statement
+
 if [ "$SOURCE_IMAGE" == "cinnamon-main" ]; then
   echo "Special configuration for cinnamon-main"
   cp /tmp/scripts/files/5275499.jpg /usr/share/backgrounds/tiles/default_blue.jpg
-  # Add commands specific to 'cinnamon-main' here
+
 elif [ "$SOURCE_IMAGE" == "kinno-main" ]; then
   echo "Special configuration for kinno-main"
-  # Add commands specific to 'kinno-main' here
+
 elif [ "$SOURCE_IMAGE" == "blue-main" ]; then
   echo "Special configuration for blue-main"
-  # Add commands specific to 'blue-main' here
+
 else
   echo "Unknown SOURCE_IMAGE: $SOURCE_IMAGE"
-  # Handle unknown or default cases here
 fi
 
 
