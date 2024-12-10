d6f87f0ed49d7b0ee98ed6cf2c50dd7d9089c0b3
murdzheff <murdzheff@gmail.com>
2024-12-10 17:13:07 +0200

    remove oh-my-bash for now

--- scripts/oh-my-bash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -ouex pipefail
-
-#move the scel folder with all required
-git clone https://github.com/ohmybash/oh-my-bash /tmp/scripts/skel/.oh-my-bash
-
-cp -r /tmp/scripts/skel /etc/
